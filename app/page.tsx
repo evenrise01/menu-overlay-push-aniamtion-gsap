21ade0e4a9646c2389d66ef847c0f9a1ea33fca0
Daksh Singh <63799338+evenrise01@users.noreply.github.com>
2025-10-24 18:22:56 +0530

    html and css styling for the animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,90 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <nav>
+        <div className="menu-bar">
+          <div className="menu-logo">
+            <a href="#">E10</a>
+          </div>
+          <div className="menu-toggle-btn">
+            <div className="menu-toggle-label">
+              <p>Menu</p>
+            </div>
+            <div className="menu-hamburger-icon">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+
+        <div className="menu-overlay">
+          <div className="menu-overlay-content">
+            <div className="menu-media-wrapper">
+              <img src="/menu-media.jpg" alt="" />
+            </div>
+            <div className="menu-content-wrapper">
+              <div className="menu-content-main">
+                <div className="menu-col">
+                  <div className="menu-link">
+                    <a href="#">Index</a>
+                  </div>
+
+                  <div className="menu-link">
+                    <a href="#">Portfolio</a>
+                  </div>
+
+                  <div className="menu-link">
+                    <a href="#">Studio</a>
+                  </div>
+
+                  <div className="menu-link">
+                    <a href="#">Journal</a>
+                  </div>
+
+                  <div className="menu-link">
+                    <a href="#">Connect</a>
+                  </div>
+                </div>
+                <div className="menu-col">
+                  <div className="menu-tag">
+                    <a href="#">Web Animations</a>
+                  </div>
+                  <div className="menu-tag">
+                    <a href="#">Interactive Media</a>
+                  </div>
+                  <div className="menu-tag">
+                    <a href="#">Motion Crafts</a>
+                  </div>
+                </div>
+              </div>
+              <div className="menu-footer">
+                <div className="menu-col">
+                  <p>Jaipur, India</p>
+                </div>
+                <div className="menu-col">
+                  <p>hello@evenrise.studios</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+      </nav>
+
+
+
+      <div className="container">
+        <section className="hero">
+          <h1>Modern design system that look timeless</h1>
+        </section>
+        <section className="banner">
+          <img src="/hero-img.jpg" alt="" />
+        </section>
+        <section className="outro">
+          <h1>Let&apos;s build something quietly iconic</h1>
+        </section>
+      </div>
+    </>
   );
 }
